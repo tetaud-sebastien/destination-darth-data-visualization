41736dd867348ca5cca3b366a7438b594a229aa8
tetaud-sebastien <tetaud.sebastien@gmail.com>
2024-05-24 06:12:06 +0000

    update

--- utils.py
+++ utils.py
@@ -10,14 +10,8 @@ from sklearn.metrics import mean_absolute_error, root_mean_squared_error
 import os 
 # Define the coordinates dictionary
 capitals_coordinates = {
-    "Tirana": (41.3275, 19.8189),
-    "Andorra la Vella": (42.5078, 1.5211),
-    "Yerevan": (40.1792, 44.4991),
     "Vienna": (48.2082, 16.3738),
-    "Baku": (40.4093, 49.8671),
-    "Minsk": (53.9045, 27.5615),
     "Brussels": (50.8503, 4.3517),
-    "Sarajevo": (43.8563, 18.4131),
     "Sofia": (42.6977, 23.3219),
     "Zagreb": (45.8150, 15.9819),
     "Nicosia": (35.1856, 33.3823),
@@ -26,43 +20,24 @@ capitals_coordinates = {
     "Tallinn": (59.4370, 24.7535),
     "Helsinki": (60.1695, 24.9355),
     "Paris": (48.8566, 2.3522),
-    "Tbilisi": (41.7151, 44.8271),
     "Berlin": (52.5200, 13.4050),
     "Athens": (37.9838, 23.7275),
     "Budapest": (47.4979, 19.0402),
-    "Reykjavik": (64.1355, -21.8954),
     "Dublin": (53.3498, -6.2603),
     "Rome": (41.9028, 12.4964),
-    "Nur-Sultan": (51.1694, 71.4491),
-    "Pristina": (42.6629, 21.1655),
     "Riga": (56.9496, 24.1052),
-    "Vaduz": (47.1410, 9.5209),
     "Vilnius": (54.6872, 25.2797),
-    "Luxembourg City": (49.6117, 6.1319),
-    "Valletta": (35.8989, 14.5146),
-    "Chișinău": (47.0105, 28.8638),
-    "Monaco": (43.7384, 7.4246),
-    "Podgorica": (42.4418, 19.2663),
+    "Luxembourg": (49.6117, 6.1319),
+    "Valletta": (35.8970, 14.5126),
     "Amsterdam": (52.3676, 4.9041),
-    "Skopje": (41.9973, 21.4280),
-    "Oslo": (59.9139, 10.7522),
     "Warsaw": (52.2297, 21.0122),
     "Lisbon": (38.7223, -9.1393),
     "Bucharest": (44.4268, 26.1025),
-    "Moscow": (55.7558, 37.6173),
-    "San Marino": (43.9333, 12.4500),
-    "Belgrade": (44.7866, 20.4489),
     "Bratislava": (48.1486, 17.1077),
     "Ljubljana": (46.0569, 14.5058),
     "Madrid": (40.4168, -3.7038),
-    "Stockholm": (59.3293, 18.0686),
-    "Bern": (46.9481, 7.4474),
-    "Ankara": (39.9334, 32.8597),
-    "Kyiv": (50.4501, 30.5234),
-    "London": (51.5074, -0.1278),
-    "Vatican City": (41.9029, 12.4534)
+    "Stockholm": (59.3293, 18.0686)
 }
-
 
 def get_cacheB_dataset(url_dataset):
            
@@ -196,28 +171,27 @@ def plot_benchmark(benchmark_dict: dict, out_dir: str):
     df = pd.DataFrame(benchmark_dict)
     df = df.T 
     df["capital"] = df.index
-    df = df.reset_index(drop=True)
     
     plt.figure(figsize=(16,8))
     sns.barplot(df, x="end_to_end", y="capital", orient="y")
     plt.title("End to End DT climate advanced benchmark")
     plt.ylabel("time [s]")
     plt.xlabel("Capital")
-    plt.legend()
-    filename = os.path.join(out_dir,"Benchmark_barplot_v1.svg")
+    plt.legend(loc='upper right')
+    filename = os.path.join(out_dir,"Benchmark_standard_barplot_v1.svg")
     plt.savefig(filename)
     
     df.drop(columns='end_to_end').plot(kind='bar', stacked=True, figsize=(10, 6))
     plt.ylabel('Time (seconds)')
     plt.title('End to End DT climate advanced benchmark')
     plt.legend(loc='upper right')
-    filename = os.path.join(out_dir,"Benchmark_barplot_v2.svg")
+    filename = os.path.join(out_dir,"Benchmark_standard_barplot_v2.svg")
     plt.savefig(filename)
     
     df.plot(kind='barh', figsize=(10, 6))
     plt.xlabel('Time (seconds)')
     plt.title('End to End DT climate advanced benchmark')
-    filename = os.path.join(out_dir,"Benchmark_barplot_v3.svg")
+    filename = os.path.join(out_dir,"Benchmark_standard_barplot_v3.svg")
     plt.savefig(filename)
     
     
