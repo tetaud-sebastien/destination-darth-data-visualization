66dc449763867e988d06656cbc633fb73c56b426
tetaud-sebastien <tetaud.sebastien@gmail.com>
2024-05-29 10:39:13 +0000

    change processing of polytope

--- polytope_test.py
+++ polytope_test.py
@@ -9,6 +9,8 @@ import earthkit.data
 import earthkit.regrid
 from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
+from tqdm import tqdm
+
 
 from utils import (get_cacheB_dataset, make_predictions, plot_benchmark,
                    plot_forecast, preprocess, train_model)
@@ -39,6 +41,8 @@ def polytope_preprocess(dataset, config: dict,lat: float = 48.8566, lon: float =
     df = pd.DataFrame(data={"time": index,
                             "temperature": dataset.values.flatten()})
     df["temperature"] = df["temperature"] - 273
+    df['time'] = pd.to_datetime(df['time'])
+
 
     return df
 
@@ -53,11 +57,17 @@ if __name__ == "__main__":
     capital_coordinates = config["capital_coordinates"]
     capital_coordinates = dict(sorted(capital_coordinates.items()))
     output_folder = config["output_folder"]
-    request_nb = config["request_nb"]
+    num_requests = config["num_requests"]
     # Generate list of dates for N days
-    N = 10  # Number of days
-    start_date = pd.to_datetime(config["polytope_request"]["date"])  # Assume the start date is provided in the config
-    dates = [start_date + pd.Timedelta(days=i) for i in range(N)]
+    N = 12  # Number of days
+    start_date = pd.to_datetime(config["polytope_request"]["date"])
+    end_date = pd.to_datetime("20211201")
+
+    # Create the date range from start date to end date
+    # date_range = [start_date + pd.Timedelta(days=i) for i in range(N)]
+
+    # Create the date range with a monthly step
+    date_range = pd.date_range(start=start_date, end=end_date, freq='MS')
 
 
     dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -68,44 +78,60 @@ if __name__ == "__main__":
     logger.info("start benchmark")
     # Iterate over each capital's coordinates for benchmarking
     for cap in capital_coordinates.keys():
+
         benchmark = {
-            "access_time": [],
-            "data_processing": [],
-            "train_model": [],
-            "model_forecast": [],
-            "end_to_end": []
+            "download_time": [None]* num_requests,
+            "data_processing": [None]* num_requests,
+            "train_model": [None]* num_requests,
+            "model_forecast": [None]* num_requests,
+            "end_to_end": [None]* num_requests,
+            "request_issues": [None]* num_requests,
             }
 
+        request_issues = 0
+
         # Repeat benchmarking for a specified number of requests
-        for _ in range(request_nb):
+        for r in tqdm(range(num_requests), desc="Processing requests", unit="request", ncols=100, colour="#3eedc4"):
 
             coord = capital_coordinates[cap]
-            logger.info(f"cap: {cap}: coord: {coord}")
+            logger.info(f"cap:{cap}: coord:{coord}")
 
-            # Initialize an empty list to store datasets
+            # Initialize an empty list to datasets grib file path
             datasets = []
+            try:
+                for i in range(len(date_range)-1):
 
-            for date in dates:
-            # Modify the polytope_request for the current date
-                config["polytope_request"]["date"] = date.strftime("%Y%m%d")
-                logger.info(config["polytope_request"]["date"])
+                    # Modify the polytope_request for the current date
 
-                # Query the data
-                t0 = time.time()
-                data = get_polytope_dataset(config=config)
-                t1 = time.time()
+                    start_date = date_range[i].strftime("%Y%m%d")
+                    end_date = date_range[i+1].strftime("%Y%m%d")
+                    date_conf = f"{start_date}/to/{end_date}"
+
+                    # config["polytope_request"]["date"] = date.strftime("%Y%m%d")
+                    config["polytope_request"]["date"] = date_conf
+                    logger.warning(config["polytope_request"]["date"])
+
+                    # Query the data
+                    t0 = time.time()
+                    data = get_polytope_dataset(config=config)
+                    t1 = time.time()
+                    datasets.append(data)
+
+            except ValueError as e:
+                logger.error(f"Issue in the data access or download: {e}")
+                request_issues += 1
+                continue  # Skip the current iteration and move to the next one
+
+            df_list = []
+            for data in datasets:
+
                 df_tmp = polytope_preprocess(dataset=data, config=config,
-                                             lat=coord[0], lon=coord[1],
-                                             method="nearest",
-                                             resample_period="D")
-                t2 = time.time()
+                                                    lat=coord[0], lon=coord[1],
+                                                    method="nearest",
+                                                    resample_period="D")
+                df_list.append(df_tmp)
 
-
-                datasets.append(df_tmp)
-            df = pd.concat(datasets, axis=0)
-            print(df)
-            df['time'] = pd.to_datetime(df['time'])
-
+            df = pd.concat(df_list, axis=0)
             t2 = time.time()
             model, train_df, test_df = train_model(df=df,
                                                    date_col='time',
@@ -120,13 +146,14 @@ if __name__ == "__main__":
                             save=True, output_path=out_dir)
             t5 = time.time()
             # Record benchmarking times
-            benchmark["access_time"].append(t1-t0)
-            benchmark["data_processing"].append(t2-t1)
-            benchmark["train_model"].append(t4-t3)
-            benchmark["model_forecast"].append(t5-t4)
-            benchmark["end_to_end"].append(t5-t0)
+            benchmark["download_time"][r]=(t1-t0)
+            benchmark["data_processing"][r]=(t2-t1)
+            benchmark["train_model"][r]=(t4-t3)
+            benchmark["model_forecast"][r]=(t5-t4)
+            benchmark["end_to_end"][r]=(t5-t0)
+            benchmark["request_issues"][r] = request_issues
 
-            #     # Calculate mean times
+            # Calculate mean times
         benchmarks[cap] = {key: np.mean(value) for key,
                            value in benchmark.items()}
         # Calculate mean and standard deviation of times
